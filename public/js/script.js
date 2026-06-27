e8e760c7d12f6caa39639227dc418f962518f32a
audreynabilla <audreynabilla006@gmail.com>
2026-06-27 10:31:18 +0700

    delete

--- public/js/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.js-confirm-delete').forEach((link) => {
-    link.addEventListener('click', (event) => {
-        if (!confirm('Yakin ingin menghapus reservasi ini?')) {
-            event.preventDefault();
-        }
-    });
-});
